c5d6e41251dca38dcd8a6e40b0de4ca590d788cc
Vinka Vladislavic <123342469+VinkaVladislavic@users.noreply.github.com>
2024-10-28 05:15:21 +0000

    solved

--- src/app.py
+++ src/app.py
@@ -15,6 +15,28 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+John = {
+    "first_name": "John",
+    "age": 33,
+    "lucky_numbers": [7,13,22]
+}
+
+Jane = {
+    "first_name": "Jane",
+    "age": 35,
+    "lucky_numbers": [10,14,3]
+}
+
+Jimmy = {
+    "first_name": "Jimmy",
+    "age": 5,
+    "lucky_numbers": [1]
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -26,17 +48,31 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
+def get_all_family_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+    return jsonify(member), 200
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def post_member():
+    body = request.get_json(force=True)
+    if not jackson_family.add_member(body):
+        return jsonify({"message": "Error adding family member"}), 400
+    return jsonify({"message": "Member added succesfully"}), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def remove_member(id):
+    selected_member = jackson_family.delete_member(id)
+    if selected_member is None:
+        return jsonify({"message": "Member not found"}), 404
+    return jsonify({"done": True}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
